3b196477352f7349a4de62d2024cf062d59cbd14
苏向夜 <fu050409@163.com>
2024-05-17 22:58:41 +0800

    feat(build): extern `COMMANDS` in `build.rs`

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
-const COMMANDS: &[&str] = &["connect"];
+const COMMANDS: &[&str] = &["connect", "close", "send_json", "recv", "entrance"];
 
 fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
+    tauri_plugin::Builder::new(COMMANDS)
+        .android_path("android")
+        .ios_path("ios")
+        .build();
 }
